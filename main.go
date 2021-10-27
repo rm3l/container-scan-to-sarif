eda7fec7df0784801d08c3556a5e457fb358436d
Armel Soro <armel@rm3l.org>
2021-10-27 08:45:08 +0200

    Handle input file and deserialize it

--- main.go
+++ main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+)
+
+type ContainerScan struct {
+	ImageName                  string                               `json:"imageName"`
+	VulnerabilityScanTimestamp string                               `json:"vulnerabilityScanTimestamp"`
+	Vulnerabilities            []ContainerScanVulnerability         `json:"vulnerabilities"`
+	BestPracticeViolations     []ContainerScanBestPracticeViolation `json:"bestPracticeViolations"`
+}
+
+type ContainerScanVulnerability struct {
+	VulnerabilityId string `json:"vulnerabilityId"`
+	PackageName     string `json:"packageName"`
+	Severity        string `json:"severity"`
+	Description     string `json:"description"`
+	Target          string `json:"target"`
+}
+
+type ContainerScanBestPracticeViolation struct {
+	Code   string `json:"code"`
+	Title  string `json:"title"`
+	Level  string `json:"level"`
+	Alerts string `json:"alerts"`
+}
 
 func main() {
-	fmt.Println("Hey")
+	inputPath := flag.String("input", "./scanreport.json", "Path to the Azure Container Scan JSON Report")
+	// outputPath := flag.String("output", "./scanreport.sarif", "Path to the SARIF output file")
+
+	flag.Parse()
+
+	input, err := ioutil.ReadFile(*inputPath)
+	if err != nil {
+		log.Fatal("Error when opening file: ", err)
+	}
+
+	var containerScan ContainerScan
+	jsonUnmarshalErr := json.Unmarshal(input, &containerScan)
+	if jsonUnmarshalErr != nil {
+		log.Fatal("Error unmarshalling JSON: ", jsonUnmarshalErr)
+	}
+	fmt.Printf("containerScan: %+v", containerScan)
 }
